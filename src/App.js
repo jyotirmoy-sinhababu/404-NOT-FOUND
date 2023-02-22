8063e655890e1e5508e7dfc72d9f9c4cf574982a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-22 23:16:08 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 
+import Nav from './components/Nav';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <p>404 NOT FOUND</p>
+    </div>
+  );
+};
+
+export default Nav;
